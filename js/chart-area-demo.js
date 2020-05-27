8f098ab4b399a45aca61f6de2c6391bff158ce1b
bensitel abdelhamid <59313545+hamidbenn@users.noreply.github.com>
2020-05-27 19:31:03 +0100

    Update chart-area-demo.js

--- js/chart-area-demo.js
+++ js/chart-area-demo.js
@@ -1,29 +1,22 @@
-  fetch('https://pomber.github.io/covid19/timeseries.json').then(function(response){
+   fetch('https://corona-api.com/timeline').then(function(response){
             return response.json();
-        }).then(function(obj){
-            var arr = new Array();
-            arr= obj["Afghanistan"];
-            var date = new Array();
-            for( var i=0;i<arr.length;i++){
-                date.push(arr[i].date);
-            }
-            fetch('https://api.coronastatistics.live/timeline/global').then(function(response){
-                return response.json();
             }).then(function(obj){
-                var cases =new Array();
-                var donnee =new Array();
-                var deaths=new Array();
-                var recovered = new Array();
-                for(var i=0 ; i<date.length;i++){
-                    donnee.push(obj[date[i]]);
-                }
-                for(var i=0 ; i<date.length;i++){
-                    cases.push(donnee[i].cases);
-                    recovered.push(donnee[i].recovered);
-                    deaths.push(donnee[i].deaths);
-                }
-                         donnee=[];
-                donnee.push(cases);
+                var arr = obj.data;
+                var confirmed =[];
+                var deaths =[];
+                var recovered =[];
+                var date = [];
+                var size =arr.length-1;
+                var j=0
+                for(var i=size;i>=0;i--){
+                    confirmed[j]=arr[i].confirmed;
+                    deaths[j]=arr[i].deaths;
+                    recovered[j]=arr[i].recovered;
+                    date[j]=arr[i].date;
+                    j++;
+                }    
+                donnee=[];
+                donnee.push(confirmed);
                 donnee.push(recovered);
                 donnee.push(deaths);
                 labels=["Cases","Recovered","Deaths"];
@@ -33,54 +26,21 @@
                     var b = Math.floor(Math.random() * 255);
                     return "rgb(" + r + "," + g + "," + b + ")";
                  };    
-                var ctxgen = document.getElementById('myAreaChart').getContext('2d');
-                var myAreaChart = new Chart(ctxgen, {
+                var ctxgen = document.getElementById('chartdiv').getContext('2d');
+                var chartgen = new Chart(ctxgen, {
                     type: 'line',
                     data: {
                         labels: date,
                         
                     },
                     options:{
-                      maintainAspectRatio: false,
                         legend:{
                             display: true,
                         }
-                    },
-                    scales: {
-      xAxes: [{
-        time: {
-          unit: 'cases'
-        },
-        gridLines: {
-          display: false,
-          drawBorder: false
-        },
-        ticks: {
-          maxTicksLimit: 6
-        },
-        maxBarThickness: 25,
-      }],
-      yAxes: [{
-        ticks: {
-          min: 0,
-          max: 15000,
-          maxTicksLimit: 5,
-          padding: 10,
-          // Include a dollar sign in the ticks
-          
-        },
-        gridLines: {
-          color: "rgb(234, 236, 244)",
-          zeroLineColor: "rgb(234, 236, 244)",
-          drawBorder: false,
-          borderDash: [2],
-          zeroLineBorderDash: [2]
-        }
-      }],
-    },
+                    }
                 });
                 for(let i=0;i<donnee.length;i++){
-                    myAreaChart.data.datasets.push({
+                    chartgen.data.datasets.push({
                         fill: false,                        
                         data: donnee[i],  
                         label: labels[i],
@@ -100,7 +60,7 @@
 
                     });
                 }
-                myAreaChart.update();
+                chartgen.update();
                 
             })
-        })
+        
